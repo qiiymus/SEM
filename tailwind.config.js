bb54774c433cf6c196f6f2e82d180de7172e8a95
CHAN YUEN FU <107622419+chanyuenfu@users.noreply.github.com>
2023-05-10 00:01:06 +0800

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        "./src/**/*.{html,js}",
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
