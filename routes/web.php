63264c7b44487435c9eee4b2f3c48e31d0f5c6e0
darksky <skynoelwee@gmail.com>
2023-06-10 16:45:11 +0800

    Update routing permissions

--- routes/web.php
+++ routes/web.php
@@ -28,18 +28,21 @@ Route::middleware([
 });
 
 // Payment Module
-// Cart
-Route::get('/cart', [PaymentController::class, 'index'])->name('cart');
-Route::post('/cart', [PaymentController::class, 'storeCart'])->name('cart.store');
-Route::delete('/cart/{id}', [PaymentController::class, 'destroyCart'])->name('cart.destroy');
-Route::post('/cart/clear', [PaymentController::class, 'destroyAll'])->name('cart.destroyAll');
-Route::get('/cart/{id}/increment', [PaymentController::class, 'incrementQuantity'])->name('cart.plus');
-Route::get('/cart/{id}/decrement', [PaymentController::class, 'decrementQuantity'])->name('cart.minus');
-// Payment
-Route::get('/cart/checkout', [PaymentController::class, 'paymentIndex'])->name('payment.pay');
-Route::post('/cart/checkout', [PaymentController::class, 'storePayment'])->name('payment.store');
-// Change
-Route::get('/change/{payment}', [PaymentController::class, 'changeIndex'])->name('payment.change');
+// Only Cashier can access this route
+Route::middleware('role:cashier')->group(function () {
+    // Cart
+    Route::get('/cart', [PaymentController::class, 'index'])->name('cart')->middleware('role:cashier');
+    Route::post('/cart', [PaymentController::class, 'storeCart'])->name('cart.store');
+    Route::delete('/cart/{id}', [PaymentController::class, 'destroyCart'])->name('cart.destroy');
+    Route::post('/cart/clear', [PaymentController::class, 'destroyAll'])->name('cart.destroyAll');
+    Route::get('/cart/{id}/increment', [PaymentController::class, 'incrementQuantity'])->name('cart.plus');
+    Route::get('/cart/{id}/decrement', [PaymentController::class, 'decrementQuantity'])->name('cart.minus');
+    // Payment
+    Route::get('/cart/checkout', [PaymentController::class, 'paymentIndex'])->name('payment.pay');
+    Route::post('/cart/checkout', [PaymentController::class, 'storePayment'])->name('payment.store');
+    // Change
+    Route::get('/change/{payment}', [PaymentController::class, 'changeIndex'])->name('payment.change');
+});
 
 // Inventory Module
 Route::get('/products', [ProductController::class, 'index'])->name('product');
@@ -50,23 +53,30 @@ Route::post('/products/update/{id}', [ProductController::class, 'update'])->name
 Route::post('/products/delete/{id}', [ProductController::class, 'destroy'])->name('deleteInventory');
 
 //Report Module
-Route::get('/report', [ReportController::class, 'index'])->name('report')->middleware('role:admin');
-Route::post('/report', [ReportController::class, 'index'])->name('report');
-Route::get('report/data/{range}', [ReportController::class, 'getData'])->name('report.data');
-Route::get('/report/export', [ReportController::class, 'exportCSV'])->name('csv');
+// Only Admin and Coordinator can access this route
+Route::middleware('role:admin,coordinator')->group(function () {
+    Route::get('/report', [ReportController::class, 'index'])->name('report')->middleware('role:admin');
+    Route::post('/report', [ReportController::class, 'index'])->name('report');
+    Route::get('report/data/{range}', [ReportController::class, 'getData'])->name('report.data');
+    Route::get('/report/export', [ReportController::class, 'exportCSV'])->name('csv');
+});
 
-// Announcement Module
-Route::get('/dashboard/announcements', [AnnouncementController::class, 'index'])->name('announcement');
-Route::get('/announcements/add', [AnnouncementController::class, 'create'])->name('addAnnouncement');
-Route::post('/announcements/store', [AnnouncementController::class, 'store'])->name('storeAnnouncement');
-Route::get('/announcements/edit/{id}', [AnnouncementController::class, 'edit'])->name('editAnnouncement');
-Route::post('/announcements/update/{id}', [AnnouncementController::class, 'update'])->name('updateAnnouncement');
-Route::post('/announcements/delete/{id}', [AnnouncementController::class, 'destroy'])->name('deleteAnnouncement');
+// Announcement & User Module
+// Only Admin can access this route
+Route::middleware('role:admin')->group(function () {
+    // Announcement Module
+    Route::get('/dashboard/announcements', [AnnouncementController::class, 'index'])->name('announcement');
+    Route::get('/announcements/add', [AnnouncementController::class, 'create'])->name('addAnnouncement');
+    Route::post('/announcements/store', [AnnouncementController::class, 'store'])->name('storeAnnouncement');
+    Route::get('/announcements/edit/{id}', [AnnouncementController::class, 'edit'])->name('editAnnouncement');
+    Route::post('/announcements/update/{id}', [AnnouncementController::class, 'update'])->name('updateAnnouncement');
+    Route::post('/announcements/delete/{id}', [AnnouncementController::class, 'destroy'])->name('deleteAnnouncement');
 
-// User Module
-Route::get('/users', [UserController::class, 'index'])->name('user');
-Route::get('/users/add', [UserController::class, 'create'])->name('addUser');
-Route::post('/users/store', [UserController::class, 'store'])->name('storeUser');
-Route::get('/users/edit', [UserController::class, 'edit'])->name('editUser');
-Route::post('/users/update', [UserController::class, 'update'])->name('updateUser');
-Route::delete('/users/delete', [UserController::class, 'destroy'])->name('deleteUser');
+    // User Module
+    Route::get('/users', [UserController::class, 'index'])->name('user');
+    Route::get('/users/add', [UserController::class, 'create'])->name('addUser');
+    Route::post('/users/store', [UserController::class, 'store'])->name('storeUser');
+    Route::get('/users/edit', [UserController::class, 'edit'])->name('editUser');
+    Route::post('/users/update', [UserController::class, 'update'])->name('updateUser');
+    Route::delete('/users/delete', [UserController::class, 'destroy'])->name('deleteUser');
+});
