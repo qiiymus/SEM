440cc91a3fedddc27776ef4448d6b070eef7fc57
darksky <skynoelwee@gmail.com>
2023-06-12 13:32:54 +0800

    update announcement view
    Co-Authored-By: jocjc00 <133036482+jocjc00@users.noreply.github.com>

--- resources/views/announcements/updateAnnouncement.blade.php
+++ resources/views/announcements/updateAnnouncement.blade.php
@@ -1,9 +1,8 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Update Announcement') }}
-        </h2>
-    </x-slot>
+    {{-- Title --}}
+    <div class="font-extrabold text-xl mt-2">
+        Update Announcement
+    </div>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,6 +1,7 @@
 <nav class="relative w-1/6 px-4 pt-8 pb-4 left-0 top-0 overflow-auto" style="background-color: #0C1446;">
     <ul class="space-y-3">
         {{-- Dashboard --}}
+        @if (Auth::user()->role != 'admin')
         <a href="{{ route('dashboard') }}">
             <x-nav-item>
                 <div class="self-center">
@@ -15,6 +16,25 @@
                 </div>
             </x-nav-item>
         </a>
+        @endif
+
+        {{-- Edit Announcement --}}
+        @if (Auth::user()->role == 'admin')
+        <a href="{{ route('announcement') }}">
+            <x-nav-item>
+                <div class="self-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+                </div>
+                <div>
+                    HOME
+                </div>
+            </x-nav-item>
+        </a>
+        @endif
 
         {{-- User List --}}
         <a href="{{ route('user') }}">
